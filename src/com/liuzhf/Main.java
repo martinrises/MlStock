51eb3cfb2fad1516495d180787dde2f0f63fd472
lzf <lzfupdate@163.com>
2016-11-27 16:00:56 +0800

    fill test data from now on

--- src/com/liuzhf/Main.java
+++ src/com/liuzhf/Main.java
@@ -22,9 +22,9 @@ public class Main {
         List<DataForSVM> dataForTraining = new ArrayList<>();
         List<DataForSVM> dataForTest = new ArrayList<>();
         int size = dataForSvmList.size();
-        int i = size * 7 / 10;
-        dataForTraining.addAll(dataForSvmList.subList(0, i));
-        dataForTest.addAll(dataForSvmList.subList(i + 1, size));
+        int i = size * 3 / 10;
+        dataForTest.addAll(dataForSvmList.subList(0, i));
+        dataForTraining.addAll(dataForSvmList.subList(i + 1, size));
 
         // scale
 //        FeatureScaleResult trainDataScaled = SvmTrainer.scaleData(dataForTraining);
